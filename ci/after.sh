32bd983142bb9de5c1810af7b3177901ef6845cc
Andrey Devyatkin <andrey.a.devyatkin@gmail.com>
2016-05-30 17:59:59 +0200

    Added deployment key for autopush to github
    Signed-off-by: Andrey Devyatkin <andrey.a.devyatkin@gmail.com>

--- ci/after.sh
+++ ci/after.sh
@@ -1,2 +1,15 @@
-git commit
-git push origin master
+git config user.name "$COMMIT_AUTHOR_NAME"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in .depkey.enc -out depkey -d
+chmod 600 depkey
+eval `ssh-agent -s`
+ssh-add depkey
+
+git push $TARGET_REPO HEAD:$TARGET_BRANCH
+git push $TRAVIS_REPO :$TRAVIS_BRANCH
--- ci/before.sh
+++ ci/before.sh
@@ -2,5 +2,5 @@ echo "Building $TRAVIS_BRANCH"
 echo "Commit: $TRAVIS_COMMIT"
 git fetch origin +refs/heads/*:refs/remotes/origin/*
 git branch -a
-git checkout origin/master
-git merge --squash $TRAVIS_COMMIT
+git checkout origin/$TARGET_BRANCH
+git merge --ff-only $TRAVIS_COMMIT
